9c1d5000f8a4f431f111397958e3127e6757aa96
nanjusoil <nanjusoil@gmail.com>
2016-03-24 18:09:33 +0800

    square finished

--- src/Square.java
+++ src/Square.java
@@ -10,39 +10,43 @@ public class Square extends Shape {
 		
 	}
 	
-	sacsc
+	
 	@Override
 	public Color getRandomColor() {		
 		// TODO Java 'Color' class takes 3 floats, from 0 to 1.
 		
 		// TODO Return color produced by three rgb floats.
-		return new Color(r, g, b);
+		  Random ran = new Random();
+		 float minX = 0.0f;
+		  float maxX = 1.0f;
+		Random rand = new Random();
+		return new Color(rand.nextFloat() * (maxX - minX) + minX, rand.nextFloat() * (maxX - minX) + minX, rand.nextFloat() * (maxX - minX) + minX);
 	}
 	
 	@Override
 	public int getShapeWidth(){
 		// TODO Return Square width
-		return 0;
+		return shapeWidth;
 	}
 	
 	@Override
 	public int getShapeHeight(){
 		// TODO Return Square width
-		return 0;
+		return shapeWidth;
 	}
 
 
 	@Override
 	public int getCenterX(int mouseX) {
 		// TODO Input mouse X position and return center X of square
-		return 0;
+		return mouseX-shapeWidth/2;
 	}
 
 
 	@Override
 	public int getCenterY(int mouseY) {
 		// TODO Input mouse Y position and return center Y of square
-		return 0;
+		return mouseX-shapeWidth/2;
 	}
 
 
